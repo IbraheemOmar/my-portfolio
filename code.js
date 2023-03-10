f1928b39e8e51bb2ae74c428ec4ec5785b67d8a3
ibruh555 <ibruh555@gmail.com>
2023-03-10 12:01:35 -0500

    version .95

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 let arrow  = document.getElementById("down-arrow");
 let counter = 0;
 
-document.addEventListener("scroll",()=>{
-    console.log(counter++);
+arrow.addEventListener("click",()=>{
+    window.scrollTo(0, document.body.getElementById("about").scrollHeight)
+    console.log(window.scrollY);
 })
 
