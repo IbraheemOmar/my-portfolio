5b36a08793fc9eee4316f512236ac77603ac6e7e
Ibraheem Omar <103038658+IbraheemOmar@users.noreply.github.com>
2023-06-16 00:06:31 -0400

    Delete code.js

--- code.js
+++
@@ -1,8 +0,0 @@
-let arrow  = document.getElementById("down-arrow");
-let counter = 0;
-
-arrow.addEventListener("click",()=>{
-    window.scrollTo(0, document.body.getElementById("about").scrollHeight)
-    console.log(window.scrollY);
-})
-
